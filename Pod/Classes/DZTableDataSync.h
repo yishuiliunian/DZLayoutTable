96b9a6ca6672a66edd1e8d5f7bc4fbbd013d21f0
stonedong <yishuiliunian@gmail.com>
2015-05-03 10:59:13 +0800

    添加状态控制和 默认的 数据刷新完成之后的处理

--- Pod/Classes/DZTableDataSync.h
+++ Pod/Classes/DZTableDataSync.h
@@ -12,12 +12,39 @@
 @interface DZTableDataSync : NSObject
 {
     NSArray* _layoutObjects;
+    NSInteger _currentPageIndex;
 }
 @property (nonatomic, weak) UITableView* tableView;
+@property (nonatomic, assign, readonly) BOOL isSyncing;
 @property (nonatomic, strong, readonly) NSArray* objects;
 - (void) insertLayoutObject:(DZLayout*)anObject atIndex:(NSUInteger)index;
-- (void) removeLayoutObject:(DZLayout*)anObject atIndex:(NSUInteger)index;
+- (void) removeLayoutObjectAtIndex:(NSUInteger)index;
 - (void) reloadData;
 - (void) syncNextObjects;
 - (DZLayout*) layoutAtIndex:(NSInteger)index;
+
+
+/**
+ *  增加pageIndex
+ */
+- (void) increasePageIndex;
+
+/**
+ *  同步状态控制,这个状态控制是可选择的，如果同步的状态不是很重要的情况下，不必加进代码中
+ */
+- (void) finishingSync;
+- (void) startSync;
+
+/**
+ *  刷新tableview上面的数据
+ *
+ *  @param datas 数据
+ */
+- (void) finishedReloadAllData:(NSArray*)datas;
+/**
+ *  在拉取下一页之后刷新数据
+ *
+ *  @param datas layoutObject的列表
+ */
+- (void) finishSyncNextDatas:(NSArray*)datas;
 @end
--- Pod/Classes/DZTableDataSync.m
+++ Pod/Classes/DZTableDataSync.m
@@ -9,6 +9,17 @@
 #import "DZTableDataSync.h"
 
 @implementation DZTableDataSync
+
+- (instancetype) init
+{
+    self = [super init];
+    if (!self) {
+        return self;
+    }
+    _currentPageIndex = 0;
+    _isSyncing = NO;
+    return self;
+}
 - (NSArray*) objects
 {
     return _layoutObjects;
@@ -20,6 +31,78 @@
 }
 - (void) reloadData
 {
+    NSAssert(NO, @"无需调用本类的该方法，该方法只是为了描述整个模型运作的机制，直接在子类重载就好了");
+}
+
+- (void) syncNextObjects
+{
+    NSAssert(NO, @"无需调用本类的该方法，该方法只是为了描述整个模型运作的机制，直接在子类重载就好了");
+}
+- (void) finishingSync
+{
+    _isSyncing = NO;
+}
+
+- (void) startSync
+{
+    _isSyncing = YES;
+}
+
+- (void) increasePageIndex
+{
+    _currentPageIndex ++;
+}
+
+- (void) finishedReloadAllData:(NSArray*)datas
+{
+    _layoutObjects = [datas copy];
+    [self.tableView reloadData];
+}
+
+- (void) finishSyncNextDatas:(NSArray*)datas
+{
+    if (datas.count == 0) {
+        return;
+    }
+    NSMutableArray* array = [_layoutObjects mutableCopy];
+    NSInteger minIndex = _layoutObjects.count;
+    [array addObjectsFromArray:datas];
+    NSInteger maxIndex = (int)array.count ;
+    _layoutObjects = array;
+    
+    NSMutableArray* indexPaths = [NSMutableArray new];
+    for (int i = (int)minIndex;  i < maxIndex; i++) {
+        NSIndexPath* path = [NSIndexPath indexPathForRow:i inSection:0];
+        [indexPaths addObject:path];
+    }
+    if (indexPaths.count == datas.count ) {
+        [self.tableView insertRowsAtIndexPaths:indexPaths withRowAnimation:UITableViewRowAnimationAutomatic];
+    }
+}
+
+- (void) insertLayoutObject:(DZLayout *)anObject atIndex:(NSUInteger)index
+{
+    if (index >= _layoutObjects.count) {
+        return;
+    }
+    
+    NSMutableArray*  insertedArray = [_layoutObjects mutableCopy];
+    [insertedArray insertObject:anObject atIndex:index];
+    _layoutObjects = insertedArray;
+    [self.tableView insertRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:index inSection:0]] withRowAnimation:UITableViewRowAnimationAutomatic];
+}
+
+- (void) removeLayoutObjectAtIndex:(NSUInteger)index
+{
+    if (index >= _layoutObjects.count) {
+        return;
+    }
+    
+    NSMutableArray * deletedArray = [_layoutObjects mutableCopy];
+    [deletedArray removeObjectAtIndex:index];
+    _layoutObjects = deletedArray;
+    
+    [self.tableView deleteRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:index inSection:0]] withRowAnimation:UITableViewRowAnimationAutomatic];
     
 }
 @end
