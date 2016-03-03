c88c42393dbdbba31d6ed05b6ca736fdd7008a3c
stonedong <yishuiliunian@gmail.com>
2016-03-03 23:54:49 +0800

    特殊版本真对 金街树

--- Pod/Classes/DZTableDataSync.m
+++ Pod/Classes/DZTableDataSync.m
@@ -7,7 +7,7 @@
 //
 
 #import "DZTableDataSync.h"
-
+#import <MJRefresh.h>
 @interface DZTableDataSync ()
 @end
 
@@ -32,9 +32,43 @@
     if (tableViewController != _tableViewController) {
         _tableViewController = tableViewController;
         if (_usingPullDownRefresh) {
-            UIRefreshControl* control = [[UIRefreshControl alloc] init];
-            [control addTarget:self action:@selector(reloadData) forControlEvents:UIControlEventValueChanged];
-            _tableViewController.refreshControl = control;
+            
+            NSArray* (^Images)(int begin ,int end) = ^(int begin ,int end) {
+                NSMutableArray* pullingImages = [NSMutableArray new];
+                for (int i = begin; i <= end; i++) {
+                    NSString* name = nil;
+                    if (i<10) {
+                        name = [NSString stringWithFormat:@"LOGOlogo 输出_0000%d",i];
+                    } else {
+                        name = [NSString stringWithFormat:@"LOGOlogo 输出_000%d",i];
+                    }
+                    
+                    UIImage* image = [UIImage imageNamed:name];
+                    if (image) {
+                        [pullingImages addObject:image];
+                    }
+                }
+                return pullingImages;
+            };
+      
+            NSArray* pullingImages = Images(0,0);
+            NSArray* refreshImages = Images(0,7);
+
+            MJRefreshGifHeader *header = [MJRefreshGifHeader headerWithRefreshingTarget:self refreshingAction:@selector(reloadData)];
+            // 设置普通状态的动画图片
+            [header setImages:pullingImages forState:MJRefreshStateIdle];
+            // 设置即将刷新状态的动画图片（一松开就会刷新的状态）
+            [header setImages:pullingImages forState:MJRefreshStatePulling];
+            // 设置正在刷新状态的动画图片
+            [header setImages:refreshImages duration:0.32 forState:MJRefreshStateRefreshing];
+            // 隐藏时间
+            header.lastUpdatedTimeLabel.hidden = YES;
+            
+            // 隐藏状态
+            header.stateLabel.hidden = YES;
+            // 设置header
+            _tableViewController.tableView.mj_header = header;
+ 
         } else {
             _tableViewController.refreshControl = nil;
         }
@@ -54,7 +88,35 @@
 - (void) finishingSync
 {
     _isSyncing = NO;
-    [self.tableViewController.refreshControl endRefreshing];
+    
+        NSArray* (^Images)(int begin ,int end) = ^(int begin ,int end) {
+            NSMutableArray* pullingImages = [NSMutableArray new];
+            for (int i = begin; i <= end; i++) {
+                NSString* name = nil;
+                if (i<10) {
+                    name = [NSString stringWithFormat:@"LOGOlogo 输出_0000%d",i];
+                } else {
+                    name = [NSString stringWithFormat:@"LOGOlogo 输出_000%d",i];
+                }
+                
+                UIImage* image = [UIImage imageNamed:name];
+                if (image) {
+                    [pullingImages addObject:image];
+                }
+            }
+            return pullingImages;
+        };
+        NSArray* idleImages = Images(8,18);
+ 
+        CGFloat time = 0.4;
+        MJRefreshGifHeader* header = (MJRefreshGifHeader*)self.tableViewController.tableView.mj_header;
+        header.gifView.animationImages = idleImages;
+        header.gifView.animationDuration = time;
+        header.gifView.animationRepeatCount = 1;
+        [header.gifView startAnimating];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(time * NSEC_PER_SEC)), dispatch_get_main_queue(), ^(void){
+            [self.tableViewController.tableView.mj_header endRefreshing];
+        });
 }
 
 - (void) startSync
